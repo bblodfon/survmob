e36116bdef2e4de16ce0797093e4a5fa9f2d13d5
john <bblodfon@gmail.com>
2023-01-07 03:22:09 +0100

    add useful code for all tests

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,1 +1,16 @@
-library(mlr3proba)
+library(mlr3proba) # loads mlr3 as well
+library(mlr3tuning)
+library(mlr3pipelines)
+library(mlr3extralearners)
+library(checkmate) # for more expect_*() functions
+
+# less logging
+lgr::get_logger('bbotk')$set_threshold('warn')
+lgr::get_logger('mlr3')$set_threshold('warn')
+
+# code/objects available to all tests
+## veteran task
+taskv = as_task_surv(x = survival::veteran,
+  time = 'time', event = 'status')
+poe = po('encode')
+taskv = poe$train(list(taskv))[[1L]]
