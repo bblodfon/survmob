000b17d626b71d2e59779e3512599f85b02ef14d
john <bblodfon@gmail.com>
2023-01-12 18:01:12 +0100

    add PipeOpRemoveNAs class and minimize_backend function + test

--- R/preprocess.R
+++ R/preprocess.R
@@ -41,3 +41,69 @@ PipeOpSurvShuffle = R6Class('PipeOpSurvShuffle',
     .predict_task = function(task) task # don't shuffle task during prediction!
   )
 )
+
+#' @title PipeOpRemoveNAs
+#' @template param_pipelines
+#' @description Removes features from a Survival Task for which the proportion
+#' of `NA` values are above a certain `cutoff` threshold
+#' @section Parameters:
+#' @param cutoff `logical(1)`\cr
+#' Features with more than `cutoff` percentage of NAs will be removed.
+#' Default value: 0.2.
+#' Higher `cutoff` values remove less features (less strict).
+#' @examples
+#' library(mlr3proba)
+#'
+#' task = tsk('lung')
+#' task
+#' pona = PipeOpRemoveNAs$new()
+#' pona$train(list(task))[[1L]] # meal.cal removed
+#'
+#' pona$param_set$values$cutoff = 0 # remove every feature with at least 1 NA
+#' pona$train(list(task))[[1L]]
+#'
+#' @export
+PipeOpRemoveNAs = R6Class('PipeOpRemoveNAs',
+  inherit = PipeOpTaskPreproc,
+  public = list(
+    #' @description
+    #' Creates a new instance of this [R6][R6::R6Class] class.
+    initialize = function(id = 'remove_na', param_vals = list()) {
+      p = ps(cutoff = p_dbl(lower = 0, upper = 1, tags = 'required'))
+      p$values = list(cutoff = 0.2)
+      super$initialize(id = id, param_set = p, param_vals = param_vals,
+        can_subset_cols = FALSE, task_type = 'TaskSurv'
+      )
+    },
+
+    #' @description How many features (columns) have NAs?
+    #' @param task [Task][mlr3::Task]
+    ncolsNA = function(task) {
+      sum(task$missings(cols = task$feature_names) > 0)
+    }
+  ),
+  private = list(
+    .train_task = function(task) {
+      pvals = self$param_set$get_values()
+      per_NA = task$missings(cols = task$feature_names)/task$nrow
+      task$select(cols = names(per_NA)[!per_NA > pvals$cutoff])
+    },
+    .predict_task = function(task) task # Do nothing during prediction
+  )
+)
+
+
+#' @title Minimize Backend of Task
+#'
+#' @description Use this function when the backend of a task hasn't changed
+#' after a lot of preprocessing, making the it to heavy to use in further
+#' modeling steps
+#'
+#' @param task [TaskSurv][mlr3proba::TaskSurv]
+#' @export
+minimize_backend = function(task) {
+  mlr3proba::as_task_surv(
+    x = task$data(), id = task$id,
+    time = 'time', event = 'status'
+  )
+}
--- tests/testthat/test-preprocess.R
+++ tests/testthat/test-preprocess.R
@@ -10,3 +10,72 @@ test_that('time-status suffling works', {
 
   expect_false(all(dt_original == dt_shuffled))
 })
+
+test_that('PipeOpRemoveNAs works', {
+  task = tsk('lung')
+  expect_equal(length(task$feature_names), 8)
+
+  pona = PipeOpRemoveNAs$new()
+  expect_equal(pona$ncolsNA(task), 6) # 6 features with at least 1 NA
+  expect_equal(pona$param_set$values$cutoff, 0.2) # default cutoff value
+
+  task2 = pona$train(list(task))[[1L]]
+  expect_equal(length(task2$feature_names), 7) # meal.cal removed
+
+  pona$param_set$values$cutoff = 0.05
+  task3 = pona$train(list(task))[[1L]]
+  expect_equal(length(task3$feature_names), 6) # meal_cal + wt_loss removed
+
+  # remove all features with at least 1 NA
+  pona$param_set$values$cutoff = 0
+  task4 = pona$train(list(task))[[1]]
+  expect_equal(length(task4$feature_names), 2)
+
+  # create another task to test what happens when all features are removed
+  df = data.frame(
+    time = c(1,2,3,4),
+    status = c(0,1,0,1),
+    X1 = c(NA,0,0,0),
+    X2 = c(NA,NA,0,0),
+    X3 = c(NA,NA,NA,0),
+    X4 = c(NA,NA,NA,NA)
+  )
+  ttask = as_task_surv(x = df, id = 'test', time = 'time', event = 'status')
+  expect_equal(pona$ncolsNA(ttask), 4)
+
+  pona$param_set$values$cutoff = 0.75
+  ttask1 = pona$train(list(ttask))[[1L]]
+  expect_equal(length(ttask1$feature_names), 3)
+
+  pona$param_set$values$cutoff = 0.5
+  ttask2 = pona$train(list(ttask))[[1L]]
+  expect_equal(length(ttask2$feature_names), 2)
+
+  pona$param_set$values$cutoff = 0.25
+  ttask3 = pona$train(list(ttask))[[1L]]
+  expect_equal(length(ttask3$feature_names), 1)
+
+  pona$param_set$values$cutoff = 0.2
+  ttask4 = pona$train(list(ttask))[[1L]]
+  expect_equal(length(ttask4$feature_names), 0)
+})
+
+test_that('minimize_backend works', {
+  task = tsk('lung')
+
+  pona = PipeOpRemoveNAs$new(param_vals = list(cutoff = 0))
+  task1 = pona$train(list(task))[[1L]]
+  expect_equal(length(task1$feature_names), 2)
+
+  # backend hasn't changed!
+  expect_equal(task1$backend$ncol, 11) # 8 feats, 2 targets (time+status) + row_id
+  expect_equal(task1$backend$nrow, 228)
+
+  # now it is
+  task2 = minimize_backend(task1)
+  expect_equal(task2$backend$ncol, 5) # 2 feats, 2 targets (time+status) + row_id
+  expect_equal(task2$backend$nrow, 228)
+
+  # original task didn't change
+  expect_equal(task1$backend$ncol, 11)
+})
