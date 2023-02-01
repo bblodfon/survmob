db60227cc8338fffc84f9cf159157c1d4af52c0f
john <bblodfon@gmail.com>
2023-02-01 09:42:55 +0100

    remove surv.oob_error, add imports from dplyr, tibble and mlr3fselect

--- R/survmob-package.R
+++ R/survmob-package.R
@@ -3,11 +3,17 @@
 
 ## usethis namespace: start
 #' @importFrom data.table data.table
+#' @importFrom dplyr bind_rows
 #' @importFrom dplyr filter
+#' @importFrom dplyr full_join
+#' @importFrom dplyr mutate
 #' @importFrom lgr get_logger
 #' @importFrom mlr3 lrn
 #' @importFrom mlr3 msr
 #' @importFrom mlr3 tsk
+#' @importFrom mlr3fselect AutoFSelector
+#' @importFrom mlr3fselect callback_fselect
+#' @importFrom mlr3fselect fs
 #' @importFrom mlr3misc open_help
 #' @importFrom mlr3pipelines %>>%
 #' @importFrom mlr3pipelines PipeOpTaskPreproc
@@ -20,13 +26,11 @@
 #' @importFrom paradox ps
 #' @importFrom parallelly availableCores
 #' @importFrom R6 R6Class
+#' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
 
 register_mlr3 = function() {
-  x = utils::getFromNamespace('mlr_measures', ns = 'mlr3')
-  x$add('surv.oob_error', MeasureSurvOOBError)
-
   x = utils::getFromNamespace('mlr_pipeops', ns = 'mlr3pipelines')
   x$add('survshuffle', PipeOpSurvShuffle)
   x$add('removenas', PipeOpRemoveNAs)
