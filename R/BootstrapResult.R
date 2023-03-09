c91b14fac0491fa18244c9674b0ff26260403c89
john <bblodfon@gmail.com>
2023-03-09 12:32:10 +0100

    add BootstrapResult Class + tests

---
+++ R/BootstrapResult.R
@@ -0,0 +1,118 @@
+#' @title Bootsrap Result Class
+#'
+#' @description mpla mpla
+#' @examples
+#' mpla mpla
+#' @export
+BootRes = R6Class('BootstrapResult',
+  public = list(
+    #' @field task_id mpla
+    task_id = NULL,
+    #' @field lrn_id mpla
+    lrn_id = NULL,
+    #' @field test_measure_ids mpla
+    test_measure_ids = NULL,
+    #' @field test_nrsmps mpla
+    test_nrsmps = NULL,
+    #' @field test_workers mpla
+    test_workers = NULL,
+    #' @field score mpla
+    score = NULL,
+    #' @field scores mpla
+    scores = NULL,
+
+    #' @description mpla mpla
+    #' @param test_measure_ids mpla
+    #' @param test_nrsmps mpla
+    #' @param test_workers mpla
+    initialize = function(test_measure_ids = bench_msrs()$id,
+      test_nrsmps = 1000, test_workers = 1) {
+      if (!all(test_measure_ids %in% bench_msrs()$id)) {
+        stop('Use only supported test measure ids from `bench_msrs()`')
+      }
+      if (test_nrsmps <= 1) {
+        stop('`test_nrsmps` needs to be larger than 1')
+      }
+      if (test_workers <= 0) {
+        stop('`test_workers` needs to be larger than 0')
+      }
+
+      self$test_measure_ids = test_measure_ids
+      self$test_nrsmps = test_nrsmps
+      self$test_workers = test_workers
+    },
+
+    #' @description Calculate bootstrapping performance scores
+    #' @param task mpla
+    #' @param learner mpla
+    #' @param part mpla
+    #' @param quiet Default: TRUE => don't report time elapsed
+    calculate = function(task, learner, part, quiet = TRUE) {
+      stopifnot('`calculate()` has already been called!' = is.null(self$scores))
+
+      assert_task(task)
+      assert_learner(learner)
+      assert_part(part)
+
+      if (is.null(learner$model)) {
+        stop('Cannot predict, learner has not been trained yet')
+      }
+
+      # assign task and learner ids
+      self$task_id = task$id
+      self$lrn_id  = learner$id
+
+      # get measures
+      measures = bench_msrs()[id %in% self$test_measure_ids]$measure
+
+      # get score on original test set
+      self$score =
+        learner$
+        predict(task, row_ids = part$test)$
+        score(measures, task = task, train_set = part$train) %>%
+        as_tibble_row()
+
+      # get scores on bootstrap test sets
+      future::plan('multisession', workers = self$test_workers)
+      tic()
+      scores = future.apply::future_lapply(1:self$test_nrsmps, function(i) {
+        # generate bootstrap test dataset
+        set.seed(i)
+        row_ids = sample(x = part$test, size = length(part$test), replace = TRUE)
+        test_data = task$data(rows = row_ids)
+
+        # predictions
+        p = learner$predict_newdata(test_data)
+
+        # scores
+        p$score(measures, task = task, train_set = part$train)
+      }, future.seed = TRUE) %>% bind_rows()
+      toc(quiet = quiet)
+      future::plan('sequential') # disable parallelization
+
+      self$scores = scores
+      invisible(scores)
+    },
+
+    #' @description Percentile confidence intervals per measure
+    #' @param conf Confidence level. Default: 0.95 (i.e. the 2.5th and 97.5th
+    #' percentile points are used to define the confidence interval range)
+    percent_ci = function(conf = 0.95) {
+      stopifnot('Run `calculate()` first'= !is.null(self$scores))
+      stopifnot(conf > 0, conf < 1)
+
+      alphas = (1 + c(-conf, conf))/2
+      apply(self$scores, 2, quantile, probs = alphas) %>%
+        as_tibble(rownames = 'perc')
+    },
+
+    #' @description Median summary score for the bootstrapping scores
+    #' (one per measure)
+    score_median = function() {
+      stopifnot('Run `calculate()` first'= !is.null(self$scores))
+
+      apply(self$scores, 2, median, na.rm = TRUE) %>%
+        as_tibble_row()
+    }
+  )
+)
---
+++ tests/testthat/test_BootstrapResult.R
@@ -0,0 +1,66 @@
+test_that('BootRes works', {
+  expect_error(BootRes$new(test_nrsmps = 1)) # too low
+  expect_error(BootRes$new(test_measure_ids = 'NonId'))
+  expect_error(BootRes$new(test_workers = 0)) # too low
+
+  boot_res = BootRes$new(test_nrsmps = 2)
+
+  # Initialization ----
+  expect_equal(length(boot_res$test_measure_ids), 5)
+  expect_equal(boot_res$test_nrsmps, 2)
+  expect_equal(boot_res$test_workers, 1) # sequential
+  expect_null(boot_res$task_id)
+  expect_null(boot_res$lrn_id)
+  expect_null(boot_res$score)
+  expect_null(boot_res$scores)
+
+  # calculate bootstrapping scores ----
+  cox = lrn('surv.coxph', id = 'cox')
+  part = partition(taskv)
+  # learner is not trained yet
+  expect_error(boot_res$calculate(task = taskv, learner = cox, part = part))
+  # bootstrapped scores have not been calculated yet
+  expect_error(boot_res$percent_ci())
+  expect_error(boot_res$score_median())
+
+  cox$train(taskv, row_ids = part$train)
+  boot_res$calculate(task = taskv, learner = cox, part = part)
+  expect_equal(boot_res$lrn_id, 'cox')
+  expect_equal(boot_res$task_id, 'veteran')
+
+  # score ----
+  score   = boot_res$score
+  expect_class(score, 'tbl_df')
+  expect_equal(colnames(score), bench_msrs()$id) # all measures
+  expect_numeric(score$ibrier)
+
+  # scores ----
+  scores  = boot_res$scores
+  expect_class(scores, 'tbl_df')
+  expect_equal(colnames(scores), bench_msrs()$id) # all measures
+  expect_numeric(scores$rcll)
+  expect_equal(dim(scores), c(2,5))
+
+  # median scores ----
+  score_median = boot_res$score_median()
+  expect_class(score_median, 'tbl_df')
+  expect_equal(colnames(score_median), bench_msrs()$id) # all measures
+  expect_numeric(score_median$dcal)
+
+  # percentile confidence intervals ----
+  per_ci = boot_res$percent_ci()
+  expect_class(per_ci, 'tbl_df')
+  expect_equal(per_ci$perc, c('2.5%', '97.5%'))
+  expect_numeric(per_ci$uno_c)
+  expect_equal(dim(boot_res$percent_ci()), c(2,6))
+
+  # 1 measure ----
+  boot_res2 = BootRes$new(test_nrsmps = 2, test_measure_ids = 'rcll')
+  boot_res2$calculate(task = taskv, learner = cox, part = part)
+  expect_class(boot_res2$scores, 'tbl_df')
+  expect_numeric(boot_res2$scores$rcll)
+  expect_numeric(boot_res2$score_median()$rcll)
+  # tidy way to get low and upper bounds for the conf. interval
+  expect_true(boot_res2$percent_ci()$rcll[1] < boot_res2$score_median()$rcll)
+  expect_true(boot_res2$percent_ci()$rcll[2] > boot_res2$score_median()$rcll)
+})
